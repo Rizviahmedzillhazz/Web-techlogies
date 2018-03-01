0edda0344d9bdb217acca67096e34d420d4885f6
Rizviahmedzillhazz <36642534+Rizviahmedzillhazz@users.noreply.github.com>
2018-03-01 14:01:13 +0600

    Add files via upload

---
+++ Lab Exam March 1 (demo)/registration.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 01, 2018 at 08:39 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 7.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `registration`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table1`
+--
+
+CREATE TABLE `table1` (
+  `Name` varchar(20) NOT NULL,
+  `Email` varchar(20) NOT NULL,
+  `User name` varchar(20) NOT NULL,
+  `Password` varchar(20) NOT NULL,
+  `Repassword` varchar(20) NOT NULL,
+  `Gender` varchar(20) NOT NULL,
+  `dob` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
